515e6e978ae9e3c00a9704e9130e1dacb95b77ee
Hsar2003 <hsar.moo@eagles.oc.edu>
2025-10-09 12:04:29 -0500

    Added limit and delete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,17 @@ import { useRouter } from "vue-router";
 
 const router = useRouter();
 const courses = ref([]);
-const message = ref("Course Homepage");
+const message = ref("My Classes");
 
+const deleteCourse = (course) => {
+  CourseServices.delete(course.id)
+    .then(() => {
+      retrieveCourses();
+    })
+    .catch((e) => {
+      message.value = e.response.data.message;
+    });
+};
 
 const retrieveCourses = () => {
   CourseServices.getAll()
@@ -26,7 +35,7 @@ retrieveCourses();
     <v-container>
      
       <v-card>
-        <v-card-title> My Classes </v-card-title>
+        <v-card-title> Course Homepage </v-card-title>
         <v-card-text>
           <b>{{ message }}</b>
         </v-card-text>
@@ -41,10 +50,18 @@ retrieveCourses();
               <tr v-for="(item, index) in courses" :key="item.title">
               <td>{{ item.dept }}</td>
               <td>{{ item.name }}</td>
-              </tr>
-            
+                <td>
+                <div style="display: flex; align-items: center;">
+                  <v-icon small class="mx-4" @click="deleteCourse(item)">
+                    mdi-trash-can
+                  </v-icon>
+                </div>
+              </td>
+              </tr> 
             </tbody>
           </v-table> 
+        <v-pagination v-model="page":length="Math.ceil(filteredCourses.length / itemsPerPage)"class="mt-4"/>
+
       </v-card>
     </v-container>
   </div>
